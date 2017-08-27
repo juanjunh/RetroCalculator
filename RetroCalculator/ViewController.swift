9ba16b28d9ae23bcaddf3415082187eddb2b5bd6
JanielHNY <janielhny@JanielHNYde-MacBook-Air.local>
2017-08-27 21:37:31 +0930

    first commit with URL not sorted yet

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,17 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
+
+
+var btnSound: AVAudioPlayer!
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.pathForResource("btn", ofType: "wav", inDirectory: "")
+        let soundURL = //can't find the corresponding code
+            
+            do{
+            try btnSound = AVAudioPlayer(contentsOfURL: <#T##NSURL#>)
+            btnSound.prepareToPlay()
+            
+            
+        }catch let err as NSError{
+            print(err.debugDescription)
+            
+        }
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPressed(sender: UIButton) {
+        playSound()
+        
+    }
+    
+    func playSound(){
+        if btnSound.playing{
+            btnSound.stop()
+        }
+        btnSound.play()
     }
 
 
