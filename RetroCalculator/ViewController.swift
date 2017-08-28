4e7a0f602737713bfd2f3ae38171fafb01279683
JanielHNY <janielhny@JanielHNYde-MacBook-Air.local>
2017-08-28 21:52:42 +0930

    second commit, //all sound coding, logic coding in progress @ 9 mins

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -10,43 +10,79 @@ import UIKit
 import AVFoundation
 
 
-var btnSound: AVAudioPlayer!
-
-
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var OutputLbl: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    enum Operation: String {
+     case Divide = "/"
+     case Add = "+"
+     case Multiply = "*"
+     case Subtract = "-"
+     case Empty = "Empty"
+    }
+    
+    
+    var currentOperation = Operation.Empty
+    var runningNumber = ""
+    var LtValStr = ""
+    var RtValStr = ""
+    var result = ""
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let path = NSBundle.pathForResource("btn", ofType: "wav", inDirectory: "")
-        let soundURL = //can't find the corresponding code
-            
-            do{
-            try btnSound = AVAudioPlayer(contentsOfURL: <#T##NSURL#>)
-            btnSound.prepareToPlay()
-            
-            
-        }catch let err as NSError{
-            print(err.debugDescription)
-            
-        }
+//        let path = NSBundle.pathForResource("btn", ofType: "wav", inDirectory: "")
+//        
+//        let soundURL = CFURLGetBaseURL(path!)
+//        
+//        
+//            do{
+//            try btnSound = AVAudioPlayer(contentsOfURL: soundURL)
+//            btnSound.prepareToPlay()
+//            
+//            
+//        }catch let err as NSError{
+//            print(err.debugDescription)
+//            
+//        }
         
         
         
     }
 
     @IBAction func numberPressed(sender: UIButton) {
-        playSound()
+       // playSound()
+        
+        runningNumber += "\(sender.tag)"
+        OutputLbl.text = runningNumber
+        
         
     }
     
-    func playSound(){
-        if btnSound.playing{
-            btnSound.stop()
+//    func playSound(){
+//        if btnSound.playing{
+//            btnSound.stop()
+//        }
+//        btnSound.play()
+//    }
+
+
+    func processOperation(operation: Operation) {
+        if currentOperation != Operation.Empty{
+            if runningNumber != "" {
+              RtValStr = runningNumber
+            }
+            
         }
-        btnSound.play()
+        
+        
+        
     }
-
-
+    
+    
 }
 
